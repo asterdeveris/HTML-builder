485fb5588b6124ff22d43a8c545f4d09ba28806c
Dasha <asterdeveris@gmail.com>
2023-05-06 23:34:01 +0300

    feat: make fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const fs = require('fs');
 
+const createFolder = async () => {
+  try {
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'), {
+      recursive: false,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readFolder = async () => {
+  try {
+    const files = await fs.promises.readdir(path.join(__dirname, 'files'));
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', `${file}`), path.join(__dirname, 'files-copy', `${file}`), (err) => {
+        if (err) return console.log(err);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createFolder();
+readFolder();
